6d2619e310a0e44367dfc805be7efc39728fd4c8
alexaaaant <saanaleks31RUS>
2019-01-28 10:50:43 +0300

    refactoring

--- frontend/src/functions/functions.js
+++ frontend/src/functions/functions.js
@@ -1,45 +1,53 @@
-export const wayToChildObj = (way, hide, withoutChild) => {
-	let steps = []
-	let string = ''
-	for (let i = 0; i < way.length; i++) {
-		if (way[i] !== '.') {
-			string = string + way[i]
-			steps.push(string)
-			steps.push('child_nodes')
-		} else {
-			string = string + way[i]
-		}
-	}
-	hide && steps.splice(-1, 1) && steps.push('hide')
+export const wayToChildObj = (route, hide, withoutChild) => {
+    let steps = []
+    let string = ''
+    for (let i = 0; i < route.length; i++) {
+        string = string + route[i]
+        route[i] !== '.' && steps.push(string) && steps.push('child_nodes')
+    }
+    hide && steps.splice(-1, 1) && steps.push('hide')
     withoutChild && steps.splice(-1, 1)
     return steps
 }
 
-export const setValue = (propertyPath, value, obj, hide, deleted, change) => {
-	if (propertyPath.length > 1) {
-		if (!obj.hasOwnProperty(propertyPath[0]) || typeof obj[propertyPath[0]] !== "object") obj[propertyPath[0]] = {}
-		if (hide) {
-			return setValue(propertyPath.slice(1), value, obj[propertyPath[0]], hide)
-		}
-		if (deleted) {
-			return setValue(propertyPath.slice(1), value, obj[propertyPath[0]], false, true)
-		}
-		if (change) {
-			return setValue(propertyPath.slice(1), value, obj[propertyPath[0]], false, false, true)
-		}
-		return setValue(propertyPath.slice(1), value, obj[propertyPath[0]])
-	} else {
-		if (hide) {
-			obj[propertyPath[0]] = !value
-
-		} else if (deleted) {
-			delete obj[propertyPath[0]]
-		} else if (change) {
-			obj[propertyPath[0]] = value
-		} else {
-			obj.loaded = true
-			obj[propertyPath[0]] = value
-		}
-		return obj
-	}
+export const addNodeToObj = (path, node, obj) => {
+    if (path.length > 1) {
+        if (!obj.hasOwnProperty(path[0]) || typeof obj[path[0]] !== "object") obj[path[0]] = {}
+        return addNodeToObj(path.slice(1), node, obj[path[0]])
+    } else {
+        obj.loaded = true
+        obj[path[0]] = node
+        return obj
+    }
 }
+
+export const changeHide = (path, currentHide, obj, hide) => {
+    if (path.length > 1) {
+        if (!obj.hasOwnProperty(path[0]) || typeof obj[path[0]] !== "object") obj[path[0]] = {}
+        return changeHide(path.slice(1), currentHide, obj[path[0]], hide)
+    } else {
+        obj[path[0]] = !currentHide
+        return obj
+    }
+}
+
+export const deleteNode = (path, obj) => {
+    if (path.length > 1) {
+        if (!obj.hasOwnProperty(path[0]) || typeof obj[path[0]] !== "object") obj[path[0]] = {}
+        return deleteNode(path.slice(1), obj[path[0]])
+    } else {
+        delete obj[path[0]]
+        return obj
+    }
+}
+
+export const changeNode = (path, node, obj) => {
+    if (path.length > 1) {
+        if (!obj.hasOwnProperty(path[0]) || typeof obj[path[0]] !== "object") obj[path[0]] = {}
+        return changeNode(path.slice(1), node, obj[path[0]])
+    } else {
+        obj[path[0]] = node
+        return obj
+    }
+}
+
--- frontend/src/store/reducers/rootReducer.js
+++ frontend/src/store/reducers/rootReducer.js
@@ -1,42 +1,38 @@
 import {GET_NODES, GET_NODE_CHILD, CHANGE_HIDE, DELETE_NODE, CHANGE_NODE, ADD_NODE} from "../../const"
-import {wayToChildObj, setValue} from "../../functions/functions"
+import {wayToChildObj, addNodeToObj, changeHide, deleteNode, changeNode} from "../../functions/functions"
 
 const initialState = ({
-	nodes: [],
+    nodes: [],
 })
 
 export function reduce(state = initialState, action) {
-	switch (action.type) {
-		case GET_NODES:
-			return {...state, nodes:action.payload}
-		case GET_NODE_CHILD:
-			const steps = wayToChildObj(action.payload.route)
-			const copyNodes = JSON.parse(JSON.stringify(state.nodes))
-			setValue(steps, action.payload.nodeChild, copyNodes)
-			return {...state, nodes: copyNodes}
-		case CHANGE_HIDE:
-			let stepsToHide = wayToChildObj(action.payload.route, true)
-			const copyAllNodes = JSON.parse(JSON.stringify(state.nodes))
-			setValue(stepsToHide, action.payload.hide, copyAllNodes, true)
-			return {...state, nodes: copyAllNodes}
-		case DELETE_NODE:
-			let stepsToDelete = wayToChildObj(action.payload, false, true)
-			const _copyAllNodes = JSON.parse(JSON.stringify(state.nodes))
-			setValue(stepsToDelete, undefined, _copyAllNodes, false, true)
-			return {...state, nodes: _copyAllNodes}
-		case CHANGE_NODE:
-			const _steps = wayToChildObj(action.payload.route, false, true)
-			const _copyNodes = JSON.parse(JSON.stringify(state.nodes))
-			setValue(_steps, action.payload.body, _copyNodes, false, false, true)
-			return {...state, nodes: _copyNodes}
-		case ADD_NODE:
-			const __steps = wayToChildObj(action.payload.newRoute, false, true)
-			const __copyNodes = JSON.parse(JSON.stringify(state.nodes))
-			setValue(__steps, action.payload.body, __copyNodes, false, false, true)
-			return {...state, nodes: __copyNodes}
-		default:
-			return state
-	}
+    let copyNodes = JSON.parse(JSON.stringify(state.nodes))
+    switch (action.type) {
+        case GET_NODES:
+            return {...state, nodes: action.payload}
+        case GET_NODE_CHILD:
+            let steps = wayToChildObj(action.payload.route)
+            addNodeToObj(steps, action.payload.nodeChild, copyNodes)
+            return {...state, nodes: copyNodes}
+        case CHANGE_HIDE:
+            let stepsToHide = wayToChildObj(action.payload.route, true)
+            changeHide(stepsToHide, action.payload.hide, copyNodes)
+            return {...state, nodes: copyNodes}
+        case DELETE_NODE:
+            let stepsToDelete = wayToChildObj(action.payload, false, true)
+            deleteNode(stepsToDelete, copyNodes)
+            return {...state, nodes: copyNodes}
+        case CHANGE_NODE:
+            let _steps = wayToChildObj(action.payload.route, false, true)
+            changeNode(_steps, action.payload.body, copyNodes)
+            return {...state, nodes: copyNodes}
+        case ADD_NODE:
+            let __steps = wayToChildObj(action.payload.newRoute, false, true)
+            changeNode(__steps, action.payload.body, copyNodes)
+            return {...state, nodes: copyNodes}
+        default:
+            return state
+    }
 }
 
 
