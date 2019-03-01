7ac2d5061ab50d4838a9ec7310f9f210a9c0938e
alexaaaant <alexaaaant@gmail.com>
2019-03-01 16:46:01 +0300

    refactoring

--- frontend/src/components/NodeInfo.js
+++ frontend/src/components/NodeInfo.js
@@ -1,43 +1,11 @@
 import React from 'react'
+import  FormComponent  from "./Form/Form"
 
 const NodeInfo = (props) => {
-  const {
-    node,
-    changeable,
-    handleChange,
-    cancelChange,
-    changeCurrNode,
-    handleSubmit
-  } = props
   return (
     <div className='node_info'>
-      <span>Узел</span>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Имя узла:
-          <input type='text' value={node.name} name='name' disabled={!changeable}
-                 onChange={handleChange}/>
-        </label>
-        <label>
-          IP-адрес:
-          <input type='text' value={node.ip} name='ip' disabled={!changeable}
-                 onChange={handleChange}/>
-        </label>
-        <label>
-          Web-порт
-          <input type='text' value={node.port} name='port' disabled={!changeable}
-                 onChange={handleChange}/>
-        </label>
-        <React.Fragment>
-          {changeable ?
-            <div>
-              <button type='submit'>Применить</button>
-              <button onClick={cancelChange}>Отменить</button>
-            </div> :
-            <button onClick={changeCurrNode}>Изменить</button>
-          }
-        </React.Fragment>
-      </form>
+          <span>Узел</span>
+          <FormComponent {...props}/>
     </div>
   )
 }
