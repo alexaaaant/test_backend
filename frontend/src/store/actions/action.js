7a94a0363255453541556cd4e77dec157b9c1708
alexaaaant <alexaaaant@gmail.com>
2019-03-01 16:44:05 +0300

    errors

--- frontend/src/store/actions/action.js
+++ frontend/src/store/actions/action.js
@@ -1,54 +1,90 @@
-import {GET_NODES, GET_NODE_CHILD, CHANGE_HIDE, DELETE_NODE, ADD_NODE, CHANGE_NODE} from "../../const"
+import { GET_NODES, GET_NODE_CHILD, CHANGE_HIDE, DELETE_NODE, ADD_NODE, CHANGE_NODE, ERROR } from "../../const"
 
 export const getNodes = (parent_id) => async dispatch => {
-	const res = await fetch(`http://localhost:3001/api/node?parent_id=${parent_id}`)
-	const nodes = await res.json()
-	if (res.ok) {
-		dispatch({type: GET_NODES, payload: nodes})
+	try {
+		const res = await fetch(`http://localhost:3001/api/node?parent_id=${parent_id}`)
+		const nodes = await res.json()
+		if (res.ok) {
+			dispatch({ type: GET_NODES, payload: nodes })
+		} else {
+			let error = await res.json()
+			dispatch({ type: ERROR, payload: error.detail })
+		}
+	} catch (e) {
+		dispatch({ type: ERROR, payload: String(e) })
 	}
 }
 
 export const getNodeChild = (node) => async dispatch => {
-	const res = await fetch(`http://localhost:3001/api/node?parent_id=${node.id}`)
-	const nodeChild = await res.json()
-	if (res.ok) {
-		dispatch({type: GET_NODE_CHILD, payload: {nodeChild: nodeChild, nodeId:node.id}})
-		dispatch({type: CHANGE_HIDE, payload: {nodeId: node.id}})
+	try {
+		const res = await fetch(`http://localhost:3001/api/node?parent_id=${node.id}`)
+		const nodeChild = await res.json()
+		if (res.ok) {
+			dispatch({ type: GET_NODE_CHILD, payload: { nodeChild: nodeChild, nodeId: node.id } })
+			dispatch({ type: CHANGE_HIDE, payload: { nodeId: node.id } })
+		} else {
+			let error = await res.json()
+			dispatch({ type: ERROR, payload: error.detail })
+		}
+	} catch (e) {
+		dispatch({ type: ERROR, payload: String(e) })
 	}
 }
 
 export const changeHide = (nodeId) => dispatch => {
-	dispatch({type: CHANGE_HIDE, payload: {nodeId:nodeId}})
+	dispatch({ type: CHANGE_HIDE, payload: { nodeId: nodeId } })
 }
 
-export const deleteNode = (nodeId,parent_id) => async dispatch => {
-	const res = await fetch(`http://localhost:3001/api/node?nodeId=${nodeId}`, {
-		method: 'DELETE'
-	})
-	if (res.ok) {
-		dispatch({type: DELETE_NODE, payload: {nodeId: nodeId, parent_id: parent_id}})
+export const deleteNode = (nodeId, parent_id) => async dispatch => {
+	try {
+		const res = await fetch(`http://localhost:3001/api/node?nodeId=${nodeId}`, {
+			method: 'DELETE'
+		})
+		if (res.ok) {
+			dispatch({ type: DELETE_NODE, payload: { nodeId: nodeId, parent_id: parent_id } })
+		} else {
+			let error = await res.json()
+			dispatch({ type: ERROR, payload: error.detail })
+		}
+	} catch (e) {
+		dispatch({ type: ERROR, payload: String(e) })
 	}
 }
 
 export const addChangedNode = (node, nodeId) => async dispatch => {
-	const res = await fetch(`http://localhost:3001/api/node?nodeId=${nodeId}`, {
-		method: 'PATCH',
-		headers: {'Content-Type': 'application/json'},
-		body: JSON.stringify(node)
-	})
-	if (res.ok) {
-    dispatch({type: CHANGE_NODE, payload: {node: node, nodeId: nodeId}})
+	try {
+		const res = await fetch(`http://localhost:3001/api/node?nodeId=${nodeId}`, {
+			method: 'PATCH',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(node)
+		})
+
+		if (res.ok) {
+			dispatch({ type: CHANGE_NODE, payload: { node: node, nodeId: nodeId } })
+		} else {
+			let error = await res.json()
+			dispatch({ type: ERROR, payload: error.detail })
+		}
+	} catch (e) {
+		dispatch({ type: ERROR, payload: String(e) })
 	}
 }
 
-export const addNode = (node, nodeId) => async dispatch=>{
-	 const res = await fetch(`http://localhost:3001/api/node?parent_id=${nodeId}`, {
-		method: 'POST',
-		headers: {'Content-Type': 'application/json'},
-		body: JSON.stringify(node)
-	})
-	if (res.ok) {
-	let node = await res.json()
-    dispatch({type: ADD_NODE, payload: {node: node, nodeId: nodeId}})
+export const addNode = (node, nodeId) => async dispatch => {
+	try {
+		const res = await fetch(`http://localhost:3001/api/node?parent_id=${nodeId}`, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(node)
+		})
+		if (res.ok) {
+			let node = await res.json()
+			dispatch({ type: ADD_NODE, payload: { node: node, nodeId: nodeId } })
+		} else {
+			let error = await res.json()
+			dispatch({ type: ERROR, payload: error.detail })
+		}
+	} catch (e) {
+		dispatch({ type: ERROR, payload: String(e) })
 	}
 }
--- frontend/src/store/reducers/rootReducer.js
+++ frontend/src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
-import { GET_NODES, GET_NODE_CHILD, CHANGE_HIDE, DELETE_NODE, ADD_NODE, CHANGE_NODE } from "../../const"
+import { GET_NODES, GET_NODE_CHILD, CHANGE_HIDE, DELETE_NODE, ADD_NODE, CHANGE_NODE, ERROR } from "../../const"
 
 const initialState = ({
     nodes: new Map(),
-    headNodes: []
+    headNodes: [],
+    error: false,
+    errorInfo: 'Ошибка'
 })
 
 export function reduce(state = initialState, action) {
@@ -10,31 +12,32 @@ export function reduce(state = initialState, action) {
     switch (action.type) {
         case GET_NODES:
             action.payload.forEach(node => copyNodes.set(node.id, node))
-            return { ...state, nodes: copyNodes, headNodes: [...copyNodes.keys()] }
+            return { ...state, nodes: copyNodes, headNodes: [...copyNodes.keys()], error: false }
         case GET_NODE_CHILD:
             copyNodes.get(action.payload.nodeId).loaded = true
             copyNodes.get(action.payload.nodeId).child_nodes = action.payload.nodeChild
             action.payload.nodeChild.forEach(node => copyNodes.set(node.id, node))
-            return { ...state, nodes: copyNodes }
+            return { ...state, nodes: copyNodes, error: false }
         case CHANGE_HIDE:
             let node = copyNodes.get(action.payload.nodeId)
             node.hide = !node.hide
-            return { ...state, nodes: copyNodes }
+            return { ...state, nodes: copyNodes, error: false }
         case DELETE_NODE:
             copyNodes.delete(action.payload.nodeId)
             let newArray = copyNodes.get(action.payload.parent_id).child_nodes.filter(item => item.id !== action.payload.nodeId)
             copyNodes.get(action.payload.parent_id).hasChild = (newArray.length > 0)
             copyNodes.get(action.payload.parent_id).child_nodes = newArray
-            return { ...state, nodes: copyNodes }
+            return { ...state, nodes: copyNodes, error: false }
         case ADD_NODE:
             copyNodes.get(action.payload.nodeId)['child_nodes'].push(action.payload.node)
             copyNodes.get(action.payload.nodeId).hasChild = true
             copyNodes.set(action.payload.node.id, action.payload.node)
-            return { ...state, nodes: copyNodes }
+            return { ...state, nodes: copyNodes, error: false }
         case CHANGE_NODE:
             Object.assign(copyNodes.get(action.payload.nodeId), action.payload.node)
-            return { ...state, nodes: copyNodes }
-
+            return { ...state, nodes: copyNodes, error: false }
+        case ERROR:
+            return { ...state, error: true, errorInfo: action.payload }
         default:
             return state
     }
